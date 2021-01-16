7e976fcaa4823dee68d0168c322a41fc4b6bcc89
mostafa4622019 <4mocpmo@gmail.com>
2021-01-16 15:10:01 +0330

    upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+string doSum(string a, string b)
+{
+    if(a.size() < b.size())
+        swap(a, b);
+
+    int j = a.size()-1;
+    for(int i=b.size()-1; i>=0; i--, j--)
+        a[j]+=(b[i]-'0');
+
+    for(int i=a.size()-1; i>0; i--)
+    {
+        if(a[i] > '9')
+        {
+            int d = a[i]-'0';
+            a[i-1] = ((a[i-1]-'0') + d/10) + '0';
+            a[i] = (d%10)+'0';
+        }
+    }
+    if(a[0] > '9')
+    {
+        string k;
+        k+=a[0];
+        a[0] = ((a[0]-'0')%10)+'0';
+        k[0] = ((k[0]-'0')/10)+'0';
+        a = k+a;
+    }
+    return a;
+}
+
+int main()
+{
+	int n,m;
+	cin>>n;
+	cin>>m;
+	
+	char narr[n];
+	char marr[m];
+	
+	cin>>narr;
+	cin>>marr;
+    cout << doSum(narr, marr)<< "\n";
+}
